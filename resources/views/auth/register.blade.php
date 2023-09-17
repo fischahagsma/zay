0ba68bc6bb240754259918b907d574dd93e12942
Fahm <fischahagsma@gmail.com>
2023-09-17 23:00:25 +0300

    add register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,29 +4,28 @@
         <div class="col-sm-5 mx-auto">
             <form method="POST" action="{{ route('register') }}">
             @csrf
+                <div class="mt-4 mb-3">
+                    <input id="name" class="form-control block mt-1 w-full" placeholder="Имя" type="text" name="name" required autofocus/>
+                </div>
+                <div class="mt-4 mb-3">
+                    <input id="lastname" class="form-control block mt-1 w-full" placeholder="Фамилия" type="text" name="lastname" />
+                </div>
+                <div class="mt-4 mb3">
+                    <input id="email" class="form-control block mt-1 w-full" placeholder="E-mail" type="email" name="email" required/>
+                </div>
+                <div class="mt-4 mb-3">
+                    <input id="password" class="form-control block mt-1 w-full"  placeholder="Пароль" type="password" name="password" required/>
+                </div>
+                <div class="mt-4 mb-3">
+                    <input id="password_confirmation" class="form-control block mt-1 w-full" placeholder="Подтверждение пароля" type="password" name="password_confirmation" required/>
+                </div>
                 <div class="mb-3">
-                    <input id="name" class="form-control block mt-1 w-full" placeholder="Имя" type="text" name="name" :value="old('name')" required autofocus/>
-                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <input type="submit" class="form-control btn btn-secondary" value="Зарегестрироваться">
                 </div>
-                <div class="mb3">
-                    <input id="email" class="form-control block mt-1 w-full" placeholder="E-mail" type="email" name="email" :value="old('email')" required/>
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                </div>
-                <div class="mt-4 mb-3">
-                    <input id="password" class="form-control block mt-1 w-full"  placeholder="пароль" type="password" name="password" required/>
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
-                <div class="mt-4 mb-3">
-                    <input id="password_confirmation" class="form-control block mt-1 w-full" placeholder="подтверждение пароля" type="password" name="password_confirmation" required/>
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                </div>
-                <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                <div class="mb-3">
+                    <a class="form-control btn btn-secondary" href="{{ route('login') }}">
                         {{ __('Already registered?') }}
                     </a>
-                    <button class="ml-4">
-                        {{ __('Register') }}
-                    </button>
                 </div>
             </form>
         </div>
