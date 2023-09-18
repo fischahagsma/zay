e2f7a86a0826cccc97d917f59f785d844fa4a266
Fahm <fischahagsma@gmail.com>
2023-09-18 21:46:15 +0300

    add profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,1 +1,38 @@
 @extends('template')
+@section('content')
+    <div class="col-sm-7 mx-auto mt-3">
+        <p><strong>Имя: </strong>{{auth()->user()->name}}</p>
+        <p><strong>Фамилия: </strong>{{auth()->user()->lastname}}</p>
+        <p><strong>E-mail: </strong>{{auth()->user()->email}}</p>
+        <p><strong>ID: </strong>{{auth()->user()->id}}</p>
+    </div>
+    <div class="col-sm-7 mx-auto mb-3">
+        <form id="logoutForm" action="/logout" method="post">
+            @csrf
+            <input class=" btn btn-secondary" type="submit" value="Выход">
+        </form>
+    </div>
+@endsection
+{{--
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-profile-information-form')
+            </div>
+        </div>
+
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-password-form')
+            </div>
+        </div>
+
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.delete-user-form')
+            </div>
+        </div>
+    </div>
+</div>
+--}}
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+Route::view('/profile', 'pages.profile')->middleware('auth');
+Route::post('/logout', [ProfileController::class, 'logout'])->middleware('auth');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/*Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 require __DIR__.'/auth.php';
